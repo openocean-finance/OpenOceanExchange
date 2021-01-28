b24609a4f72f985694039f8ff8997d58477ccf2d
LightKool <lq.rockfeller@gmail.com>
2021-01-28 17:48:25 +0800

    test environment update

--- truffle-config.js
+++ truffle-config.js
@@ -13,7 +13,7 @@ module.exports = {
             networkCheckTimeout: 100000000
         },
         test: {
-            host: "47.242.20.196",
+            host: "47.242.129.229",
             port: 8545,
             network_id: "1",
             networkCheckTimeout: 100000000
