a405225411da549806c514ca46157318cabbca1a
LightKool <lq.rockfeller@gmail.com>
2021-08-26 10:43:14 +0800

    bug fix

--- contracts/dexes/IBXHash.sol
+++ contracts/dexes/IBXHash.sol
@@ -80,10 +80,10 @@ library IBXHashPairExtension {
         uint256 outReserve,
         uint256 amount
     ) private pure returns (uint256) {
-        uint256 inAmountWithFee = amount.mul(99775);
+        uint256 inAmountWithFee = amount.mul(997);
         // Uniswap V2 now requires fixed 0.3% swap fee
         uint256 numerator = inAmountWithFee.mul(outReserve);
-        uint256 denominator = inReserve.mul(100000).add(inAmountWithFee);
+        uint256 denominator = inReserve.mul(1000).add(inAmountWithFee);
         return (denominator == 0) ? 0 : numerator.div(denominator);
     }
 }
