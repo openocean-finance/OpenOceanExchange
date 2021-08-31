8b23003cc6403d5314ebf7d837b7ec0c6e1d6466
LightKool <lq.rockfeller@gmail.com>
2021-08-31 13:40:23 +0800

    bug fix

--- DexOneAll.full.sol
+++ DexOneAll.full.sol
@@ -1572,10 +1572,6 @@ contract DexOne is IDexOne {
             return amount;
         }
 
-        uint256 senderBalance = inToken.universalBalanceOf(msg.sender);
-        if (senderBalance < amount) {
-            amount = senderBalance;
-        }
         inToken.universalTransferFrom(msg.sender, address(this), amount);
         uint256 balance = inToken.universalBalanceOf(address(this));
 
